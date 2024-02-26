6aaaf137729a2dada82a04b2316c1d0a8e24710e
Jawad-ICM <jawad@icaremanager.com>
2024-02-26 13:23:16 +0500

    basic project and problem of creating multiple objects

---
+++ Creational/SingletonDemo/SingletonDemo/Board.cs
@@ -0,0 +1,21 @@
+﻿namespace SingletonDemo;
+
+public class Board
+{
+    public string BoardName { get; set; }
+    public Board(string _boardName)
+    {
+        BoardName = _boardName;
+        Console.WriteLine($"Connecting to {_boardName}");
+    }
+
+    public void Execute(string _programName)
+    {
+        Console.WriteLine($"Executing Program {_programName}");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(i.ToString());
+        }
+    }
+
+}
---
+++ Creational/SingletonDemo/SingletonDemo/Program.cs
@@ -0,0 +1,8 @@
+﻿using SingletonDemo;
+
+var board1 = new Board("Jawad Board");
+board1.Execute("program 1");
+
+
+var board2 = new Board("Hamza Board");
+board2.Execute("Program 2");
