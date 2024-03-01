5daed7b7e051e3043c08b13edb2373daaf0d04e0
Jawad-ICM <jawad@icaremanager.com>
2024-03-01 14:15:51 +0500

    basic solution
    implement the solution for Forex Exchange

---
+++ Creational/FactoryDemo/FactoryDemo/Forex.cs
@@ -0,0 +1,31 @@
+﻿using FactoryDemo.Models;
+using FactoryDemo.Processors;
+
+namespace FactoryDemo;
+
+public class Forex
+{
+    private Exchange _exchange;
+    public Forex(Exchange exchange)
+    {
+        _exchange = exchange;
+    }
+
+    public List<double> GetForcastData()
+    {
+        IProcessor processor = null;
+        if (_exchange.Name == "BTC")
+        {
+            processor = new BitcoinProcessor(Risk.High, _exchange.Volume_1day_usd);
+        }
+        else if (_exchange.Name == "ETH")
+        {
+            processor = new BitcoinProcessor(Risk.High, _exchange.Volume_1day_usd);
+        }
+        else
+        {
+            processor = new BitcoinProcessor(Risk.Low, _exchange.Volume_1hrs_usd);
+        }
+        return processor.Predict();
+    }
+}
---
+++ Creational/FactoryDemo/FactoryDemo/Processors/EtherumProcessor.cs
@@ -0,0 +1,58 @@
+﻿using FactoryDemo.Models;
+using System;
+
+namespace FactoryDemo.Processors;
+
+public class EtherumProcessor : IProcessor
+{
+    private float volume_1day_usd;
+    public Risk risk { get; set; }
+    public BitcoinProcessor(Risk risk, float volume_1day_usd)
+    {
+        this.volume_1day_usd = volume_1day_usd;
+    }
+    public List<double> Predict()
+    {
+        if (risk == Risk.High)
+        {
+            return new List<double>()
+            {
+                287.3,
+                281.4,
+                240.2,
+                229.6,
+                250.2,
+                210.1,
+                218.5
+            };
+        }
+        else if (risk == Risk.Medium)
+        {
+            return new List<double>()
+            {
+                210.4,
+                213.2,
+                213.8,
+                214.1,
+                217.3,
+                218.4,
+                218.7,
+                220.3
+            };
+        }
+        else
+        {
+            return new List<double>()
+            {
+                110.4,
+                113.2,
+                113.8,
+                114.1,
+                117.3,
+                118.4,
+                118.7,
+                120.3
+            };
+        }
+    }
+}
